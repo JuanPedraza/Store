918fde5b9a7e61ca89b88498429a9c29e23441b8
JuanPedraza <dark2022@gmail.com>
2018-10-26 20:58:47 -0500

    Añadiendo más servicios

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* Producto */
+/* Producto - Post */
 router.post('/save', (req, res, next) => {
 
   const nuevoProducto = {
@@ -10,10 +10,34 @@ router.post('/save', (req, res, next) => {
   }
 
   models.Product.create(nuevoProducto)
-  .then(elNuevoProducto =>{
-    res.json(elNuevoProducto);
-  });
+    .then(elNuevoProducto => {
+      res.json(elNuevoProducto);
+    });
 
 });
+
+
+/* Producto - PUT */
+
+
+router.put('/save/:id', (req, res, next) => {
+
+  models.Product.findOne({
+    where: {id: req.params.id}
+  }).then(product => {
+    product.nombre = req.query.nombre;
+    product.description = req.query.descripcion;
+
+    product.save()
+      .then(p => {
+        res.json(p);
+      })
+
+  });
+
+ 
+});
+
+
 
 module.exports = router;
